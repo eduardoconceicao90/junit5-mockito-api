38c2a83beec62b9bce694caa1162d7fa14beb7ab
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-07-01 11:32:42 -0300

    first commit

---
+++ src/main/java/io/com/github/eduardoconceicao90/junit5_mockito_api/Junit5MockitoApiApplication.java
@@ -0,0 +1,13 @@
+package io.com.github.eduardoconceicao90.junit5_mockito_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5MockitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5MockitoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/com/github/eduardoconceicao90/junit5_mockito_api/Junit5MockitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.com.github.eduardoconceicao90.junit5_mockito_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5MockitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
