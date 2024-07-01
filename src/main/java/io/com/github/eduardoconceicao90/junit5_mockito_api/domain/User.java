43067fb049db29b8a77cc60620144a91a7d86723
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-07-01 12:03:34 -0300

    Endpoint findById

---
+++ src/main/java/io/com/github/eduardoconceicao90/junit5_mockito_api/domain/User.java
@@ -0,0 +1,27 @@
+package io.com.github.eduardoconceicao90.junit5_mockito_api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
