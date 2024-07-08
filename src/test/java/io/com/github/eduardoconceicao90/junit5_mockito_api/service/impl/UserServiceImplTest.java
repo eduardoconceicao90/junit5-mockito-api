e8397fee20b94a5953f5eac4045e5484a3bc931a
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-07-08 09:48:21 -0300

    Teste UserService update com sucesso

--- src/test/java/io/com/github/eduardoconceicao90/junit5_mockito_api/service/impl/UserServiceImplTest.java
+++ src/test/java/io/com/github/eduardoconceicao90/junit5_mockito_api/service/impl/UserServiceImplTest.java
@@ -116,7 +116,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+
+        User response = userService.update(userDTO);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(User.class, response.getClass());
+        Assertions.assertEquals(ID, response.getId());
+        Assertions.assertEquals(NAME, response.getName());
+        Assertions.assertEquals(EMAIL, response.getEmail());
+        Assertions.assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
